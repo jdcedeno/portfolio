9baed8f4ef12200ac61b2541d5b3f17cf9864099
JDCedeno <56703919+jdcedeno@users.noreply.github.com>
2019-10-30 20:37:06 -0500

    Updates

---
+++ precache-manifest.5c29ceac46ec04fda3831e8f9d57efb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1476445356d1bdd06912196032b56d",
+    "url": "/jdcedeno/jdcedeno.github.io.git/index.html"
+  },
+  {
+    "revision": "cc37cc3ee0ce35bd2e86",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bb34de4e5ca85c6f9180",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/css/main.1da6843f.chunk.css"
+  },
+  {
+    "revision": "cc37cc3ee0ce35bd2e86",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/js/2.bd8b79eb.chunk.js"
+  },
+  {
+    "revision": "bb34de4e5ca85c6f9180",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/js/main.55aae3f5.chunk.js"
+  },
+  {
+    "revision": "3afe5cb5b800f039311a",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/js/runtime-main.89a46dc5.js"
+  },
+  {
+    "revision": "fdf0c4be6fbc7961c0beefc28056c36f",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/media/JCLogo3.fdf0c4be.svg"
+  },
+  {
+    "revision": "dbe2a52bb7b98094d518f1d2ccd91c43",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/media/Logo3C.dbe2a52b.svg"
+  },
+  {
+    "revision": "e29fd8057b3af60c5eb696eae0aad71f",
+    "url": "/jdcedeno/jdcedeno.github.io.git/static/media/Logo3J.e29fd805.svg"
+  }
+]);
